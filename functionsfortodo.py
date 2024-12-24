50adcd872491730b5c45eaf8f9ce6525bc0afed2
pradgit <phadnispradnya@gmail.com>
2024-12-24 08:01:38 +0530

    Update functionsfortodo.py
    Change done on date by person...

--- functionsfortodo.py
+++ functionsfortodo.py
@@ -1,5 +1,6 @@
 FILEPATH = 'todos.txt'
 
+#Added comment for reqcycle1
 def get_todos(filepath=FILEPATH):
     with open(filepath,'r') as file_local:
         todos_local = file_local.readlines()
@@ -8,5 +9,8 @@ def get_todos(filepath=FILEPATH):
 def write_todos(newtodo,filepath=FILEPATH):
     with open(filepath,'w') as file_write:
         file_write.writelines(newtodo)
+
+def newlogichere():
+    pass
 
 
