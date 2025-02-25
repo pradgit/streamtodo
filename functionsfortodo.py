d3a3707f57cd26711629eabe4a5c904559628ab1
pradgit <phadnispradnya@gmail.com>
2025-02-25 08:18:26 +0530

    Update functionsfortodo.py
    Sprint1 - Made changes in the comments section and added print statement

--- functionsfortodo.py
+++ functionsfortodo.py
@@ -1,13 +1,15 @@
 FILEPATH = 'todos.txt'
 
 #Added comment for reqcycle1
+#Added new comments for the function - chatchanges branch
 def get_todos(filepath=FILEPATH):
     with open(filepath,'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
-
+#Added new comments for the function
 def write_todos(newtodo,filepath=FILEPATH):
     with open(filepath,'w') as file_write:
+        print('hello')
         file_write.writelines(newtodo)
 
 def newlogichere():
